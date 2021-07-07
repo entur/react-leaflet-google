61f6d4dc86c698756752989aaf18093195477c5a
Mylothros-Charalampos <charalampos.mylothros@navd.ai>
2021-07-07 14:10:34 +0300

    fix: rebuild project

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,48 @@ module.exports = {
     library: 'ReactLeaflet',
     libraryTarget: 'umd'
   },
+  externals: [
+    {
+      leaflet: {
+        amd: 'leaflet',
+        commonjs: 'leaflet',
+        commonjs2: 'leaflet',
+        root: 'L'
+      },
+    },
+    {
+      jquery: {
+        amd: 'jquery',
+        commonjs: 'jquery',
+        commonjs2: 'jquery',
+        root: 'JQuery'
+      }
+    },
+    {
+      'react-leaflet': {
+        amd: 'react-leaflet',
+        commonjs: 'react-leaflet',
+        commonjs2: 'react-leaflet'
+      }
+    },
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      }
+    },
+    {
+      'google-maps': {
+        amd: 'google-maps',
+        commonjs: 'google-maps',
+        commonjs2: 'google-maps',
+        root: 'GoogleMapsLoader'
+      }
+    }
+  ],
+  mode: "production",
   module: {
 		rules: [
             {
